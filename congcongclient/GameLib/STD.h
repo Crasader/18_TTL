7a6be060704857c747007c8d8ffdbace3ade468c
taotingfu <taotingfu819@163.com>
2018-12-11 11:54:27 +0800

    [add]STD.h

---
+++ congcongclient/GameLib/STD.h
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <algorithm>
+#include <deque>
+#include <iomanip>
+#include <list>
+#include <sstream>
+#include <vector>
+#include <map>
+
+#ifndef ASSERT
+#define ASSERT(f) assert(f)
+#endif
