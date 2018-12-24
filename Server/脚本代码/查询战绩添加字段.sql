59cd64021730014babb18495f5a8a454e3d5459f
taotingfu <taotingfu819@163.com>
2018-12-24 13:25:38 +0800

    [modify]修改了请求战绩的接口

--- Server/脚本代码/查询战绩添加字段.sql
+++ Server/脚本代码/查询战绩添加字段.sql
@@ -1,4 +1,26 @@
 USE QPTreasureDB
 GO
+/*
+ALTER TABLE [dbo].RecordDrawInfo DROP COLUMN BaseScore
+ALTER TABLE [dbo].RecordDrawInfo ADD BaseScore BIGINT NOT NULL default 0
 
-ALTER TABLE [dbo].RecordDrawScore ADD TableID INT NOT NULL default 0
+ALTER TABLE [dbo].RecordDrawInfo DROP COLUMN RulesBytes
+ALTER TABLE [dbo].RecordDrawInfo ADD RulesBytes BIGINT NOT NULL default 0
+
+ALTER TABLE [dbo].RecordDrawInfo DROP COLUMN InSertTime
+ALTER TABLE [dbo].RecordDrawInfo ADD InSertTime BIGINT NOT NULL default 0
+
+ALTER TABLE [dbo].RecordDrawInfo DROP COLUMN StartTime
+ALTER TABLE [dbo].RecordDrawInfo ADD StartTime BIGINT NOT NULL default 0
+
+ALTER TABLE [dbo].RecordDrawInfo DROP COLUMN ConcludeTime
+ALTER TABLE [dbo].RecordDrawInfo ADD ConcludeTime BIGINT NOT NULL default 0
+
+ALTER TABLE [dbo].RecordDrawScore DROP COLUMN InSertTime
+ALTER TABLE [dbo].RecordDrawScore ADD InSertTime BIGINT NOT NULL default 0
+
+*/
+
+ALTER TABLE [dbo].RecordDrawScore ADD StartTime BIGINT NOT NULL default 0
+
+GO
